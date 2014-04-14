a8b64956ecafa0f7868dd073e8b502c713e72ea8
Adam Wathan <adam.wathan@gmail.com>
2014-04-14 08:54:57 -0400

    Make FunctionalTestCase abstract so PHPUnit doesn't try to run it on it's own

--- tests/FunctionalTestCase.php
+++ tests/FunctionalTestCase.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
-class FunctionalTestCase extends PHPUnit_Framework_TestCase
+abstract class FunctionalTestCase extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
