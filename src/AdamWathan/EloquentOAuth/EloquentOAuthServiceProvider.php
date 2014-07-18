d2bf6cee0688538a45f1bc5b36155e0b89b44d9f
Frank Klaassen <frank@frankklaassen.nl>
2014-07-18 08:16:33 +0200

    indentation

--- src/AdamWathan/EloquentOAuth/EloquentOAuthServiceProvider.php
+++ src/AdamWathan/EloquentOAuth/EloquentOAuthServiceProvider.php
@@ -5,78 +5,78 @@ use Guzzle\Http\Client as HttpClient;
 
 class EloquentOAuthServiceProvider extends ServiceProvider {
 
-	protected $providerLookup = array(
-		'facebook' => 'AdamWathan\\EloquentOAuth\\Providers\\FacebookProvider',
-		'github' => 'AdamWathan\\EloquentOAuth\\Providers\\GitHubProvider',
-		'google' => 'AdamWathan\\EloquentOAuth\\Providers\\GoogleProvider',
-		'linkedin' => 'AdamWathan\\EloquentOAuth\\Providers\\LinkedInProvider',
-		);
+    protected $providerLookup = array(
+        'facebook' => 'AdamWathan\\EloquentOAuth\\Providers\\FacebookProvider',
+        'github' => 'AdamWathan\\EloquentOAuth\\Providers\\GitHubProvider',
+        'google' => 'AdamWathan\\EloquentOAuth\\Providers\\GoogleProvider',
+        'linkedin' => 'AdamWathan\\EloquentOAuth\\Providers\\LinkedInProvider',
+    );
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('adamwathan/eloquent-oauth');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('adamwathan/eloquent-oauth');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->registerOAuthManager();
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerOAuthManager();
+    }
 
-	protected function registerOAuthManager()
-	{
-		$this->app['adamwathan.oauth'] = $this->app->share(function($app)
-		{
-			$this->configureOAuthIdentitiesTable();
-			$users = new UserStore($app['config']['auth.model']);
-			$stateManager = new StateManager($app['session.store'], $app['request']);
-			$oauth = new OAuthManager($app['auth'], $app['redirect'], $stateManager, $users, new IdentityStore);
-			$this->registerProviders($oauth);
-			return $oauth;
-		});
-	}
+    protected function registerOAuthManager()
+    {
+        $this->app['adamwathan.oauth'] = $this->app->share(function($app)
+    {
+        $this->configureOAuthIdentitiesTable();
+        $users = new UserStore($app['config']['auth.model']);
+        $stateManager = new StateManager($app['session.store'], $app['request']);
+        $oauth = new OAuthManager($app['auth'], $app['redirect'], $stateManager, $users, new IdentityStore);
+        $this->registerProviders($oauth);
+        return $oauth;
+    });
+    }
 
-	protected function registerProviders($oauth)
-	{
-		$providerAliases = $this->app['config']['eloquent-oauth::providers'];
+    protected function registerProviders($oauth)
+    {
+        $providerAliases = $this->app['config']['eloquent-oauth::providers'];
         foreach ($providerAliases as $alias => $config) {
             if(isset($this->providerLookup[$alias])) {
-    			$providerClass = $this->providerLookup[$alias];
-	    		$provider = new $providerClass($config, new HttpClient, $this->app['request']);
-		    	$oauth->registerProvider($alias, $provider);
+                $providerClass = $this->providerLookup[$alias];
+                $provider = new $providerClass($config, new HttpClient, $this->app['request']);
+                $oauth->registerProvider($alias, $provider);
             }
-		}
-	}
+        }
+    }
 
-	protected function configureOAuthIdentitiesTable()
-	{
-		OAuthIdentity::configureTable($this->app['config']['eloquent-oauth::table']);
-	}
+    protected function configureOAuthIdentitiesTable()
+    {
+        OAuthIdentity::configureTable($this->app['config']['eloquent-oauth::table']);
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('adamwathan.oauth');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('adamwathan.oauth');
+    }
 
 }
