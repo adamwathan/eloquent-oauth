10f8e706041c820bd812696d75f7a39325ee1911
Adam Wathan <adam.wathan@gmail.com>
2015-03-22 13:24:46 -0400

    Add basic test for OAuthManager::authorize()

--- tests/OAuthManagerTest.php
+++ tests/OAuthManagerTest.php
@@ -2,6 +2,7 @@
 
 use Mockery as M;
 use AdamWathan\EloquentOAuth\OAuthManager;
+use Illuminate\Http\RedirectResponse;
 
 class OAuthManagerTest extends PHPUnit_Framework_TestCase
 {
@@ -10,5 +11,23 @@ class OAuthManagerTest extends PHPUnit_Framework_TestCase
         M::close();
     }
 
-    public function test_placeholder() {}
+    public function test_it_returns_a_redirect_to_the_authorize_url()
+    {
+        $redirector = new SimpleRedirector;
+        $authenticator = M::mock('AdamWathan\EloquentOAuth\Authenticator');
+        $socialnorm = M::mock('SocialNorm\SocialNorm');
+        $socialnorm->shouldReceive('authorize')->with('example')->andReturn('http://example.com/authorize');
+
+        $oauth = new OAuthManager($redirector, $authenticator, $socialnorm);
+        $response = $oauth->authorize('example');
+        $this->assertEquals('http://example.com/authorize', $response->getTargetUrl());
+    }
+}
+
+class SimpleRedirector
+{
+    public function to($path)
+    {
+        return new RedirectResponse($path);
+    }
 }
