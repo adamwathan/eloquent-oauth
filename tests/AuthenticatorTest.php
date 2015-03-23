b2c7d05b7528a139a249295525a978396341513c
Adam Wathan <adam.wathan@gmail.com>
2015-03-23 08:40:47 -0400

    Improve Authenticator tests

--- tests/AuthenticatorTest.php
+++ tests/AuthenticatorTest.php
@@ -3,6 +3,7 @@
 use Mockery as M;
 use AdamWathan\EloquentOAuth\Authenticator;
 use AdamWathan\EloquentOAuth\OAuthIdentity;
+use SocialNorm\User as SocialNormUser;
 
 class AuthenticatorTest extends PHPUnit_Framework_TestCase
 {
@@ -13,42 +14,44 @@ class AuthenticatorTest extends PHPUnit_Framework_TestCase
 
     public function test_login_creates_new_user_if_no_matching_user_exists()
     {
-        $auth = M::mock('Illuminate\Contracts\Auth\Guard');
-        $users  = M::mock('AdamWathan\EloquentOAuth\UserStore');
-        $identities  = M::mock('AdamWathan\EloquentOAuth\IdentityStore')->shouldIgnoreMissing();
-
-        $userDetails = M::mock('SocialNorm\User');
-
+        $providerAlias = 'provider';
+        $auth = M::spy();
+        $users  = M::spy();
+        $identities  = M::spy();
+        $userDetails = new SocialNormUser([]);
         $user = M::mock('Illuminate\Contracts\Auth\Authenticatable')->shouldIgnoreMissing();
 
-        $authenticator = new Authenticator($auth, $users, $identities);
-
+        $identities->shouldReceive('userExists')->andReturn(false);
         $users->shouldReceive('create')->andReturn($user);
-        $users->shouldReceive('store')->once();
-        $auth->shouldReceive('login')->with($user)->once();
 
+        $authenticator = new Authenticator($auth, $users, $identities);
         $authenticator->login('provider', $userDetails);
+
+        $users->shouldHaveReceived('create');
+        $users->shouldHaveReceived('store')->with($user);
+        $identities->shouldHaveReceived('store');
+        $auth->shouldHaveReceived('login')->with($user);
     }
 
     public function test_login_uses_existing_user_if_matching_user_exists()
     {
-        $auth = M::mock('Illuminate\Contracts\Auth\Guard');
-        $users  = M::mock('AdamWathan\EloquentOAuth\UserStore')->shouldIgnoreMissing();
-        $identities  = M::mock('AdamWathan\EloquentOAuth\IdentityStore')->shouldIgnoreMissing();
-
-        $userDetails = M::mock('SocialNorm\User');
-        $identity = new OAuthIdentity;
-
+        $providerAlias = 'provider';
+        $auth = M::spy();
+        $users  = M::spy();
+        $identities  = M::spy();
+        $userDetails = new SocialNormUser([]);
         $user = M::mock('Illuminate\Contracts\Auth\Authenticatable')->shouldIgnoreMissing();
 
-        $authenticator = new Authenticator($auth, $users, $identities);
-
         $identities->shouldReceive('userExists')->andReturn(true);
-        $identities->shouldReceive('getByProvider')->andReturn($identity);
-        $users->shouldReceive('create')->never();
+        $identities->shouldReceive('getByProvider')->andReturn(new OAuthIdentity);
         $users->shouldReceive('findByIdentity')->andReturn($user);
-        $auth->shouldReceive('login')->with($user)->once();
 
+        $authenticator = new Authenticator($auth, $users, $identities);
         $authenticator->login('provider', $userDetails);
+
+        $users->shouldNotHaveReceived('create');
+        $users->shouldHaveReceived('store')->with($user);
+        $identities->shouldHaveReceived('store');
+        $auth->shouldHaveReceived('login')->with($user);
     }
 }
