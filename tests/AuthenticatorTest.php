ab5b1ac194e1246751da6b38d7f98b738c98061c
Adam Wathan <adam.wathan@gmail.com>
2015-10-26 09:57:03 -0400

    Allow overriding user by returning a user from the callback

--- tests/AuthenticatorTest.php
+++ tests/AuthenticatorTest.php
@@ -36,15 +36,20 @@ class AuthenticatorTest extends PHPUnit_Framework_TestCase
     public function test_login_uses_existing_user_if_matching_user_exists()
     {
         $providerAlias = 'provider';
-        $auth = M::spy();
-        $users  = M::spy();
-        $identities  = M::spy();
+
         $userDetails = new SocialNormUser([]);
         $user = M::mock('Illuminate\Contracts\Auth\Authenticatable')->shouldIgnoreMissing();
 
-        $identities->shouldReceive('userExists')->andReturn(true);
-        $identities->shouldReceive('getByProvider')->andReturn(new OAuthIdentity);
-        $users->shouldReceive('findByIdentity')->andReturn($user);
+        $auth = M::spy();
+
+        $users  = M::spy([
+            'findByIdentity' => $user
+        ]);
+
+        $identities  = M::spy([
+            'userExists' => true,
+            'getByProvider' => new OAuthIdentity,
+        ]);
 
         $authenticator = new Authenticator($auth, $users, $identities);
         $authenticator->login('provider', $userDetails);
@@ -53,5 +58,65 @@ class AuthenticatorTest extends PHPUnit_Framework_TestCase
         $users->shouldHaveReceived('store')->with($user);
         $identities->shouldHaveReceived('store');
         $auth->shouldHaveReceived('login')->with($user);
+    }
+
+    public function test_if_a_user_is_returned_from_the_callback_that_user_is_used()
+    {
+        $providerAlias = 'provider';
+
+        $userDetails = new SocialNormUser([]);
+        $user = M::mock('Illuminate\Contracts\Auth\Authenticatable')->shouldIgnoreMissing();
+        $otherUser = M::mock('Illuminate\Contracts\Auth\Authenticatable')->shouldIgnoreMissing();
+
+        $auth = M::spy();
+
+        $users  = M::spy([
+            'findByIdentity' => $user
+        ]);
+
+        $identities  = M::spy([
+            'userExists' => true,
+            'getByProvider' => new OAuthIdentity,
+        ]);
+
+        $authenticator = new Authenticator($auth, $users, $identities);
+        $authenticator->login('provider', $userDetails, function () use ($otherUser) {
+            return $otherUser;
+        });
+
+        $users->shouldNotHaveReceived('create');
+        $users->shouldHaveReceived('store')->with($otherUser);
+        $identities->shouldHaveReceived('store');
+        $auth->shouldHaveReceived('login')->with($otherUser);
+    }
+
+    public function test_if_nothing_is_returned_from_the_callback_the_found_or_created_user_is_used()
+    {
+        $providerAlias = 'provider';
+
+        $userDetails = new SocialNormUser([]);
+        $foundUser = M::mock('Illuminate\Contracts\Auth\Authenticatable')->shouldIgnoreMissing();
+        $otherUser = M::mock('Illuminate\Contracts\Auth\Authenticatable')->shouldIgnoreMissing();
+
+        $auth = M::spy();
+
+        $users  = M::spy([
+            'findByIdentity' => $foundUser
+        ]);
+
+        $identities  = M::spy([
+            'userExists' => true,
+            'getByProvider' => new OAuthIdentity,
+        ]);
+
+        $authenticator = new Authenticator($auth, $users, $identities);
+        $authenticator->login('provider', $userDetails, function () {
+            return;
+        });
+
+        $users->shouldNotHaveReceived('create');
+        $users->shouldHaveReceived('store')->with($foundUser);
+        $identities->shouldHaveReceived('store');
+        $auth->shouldHaveReceived('login')->with($foundUser);
     }
 }
